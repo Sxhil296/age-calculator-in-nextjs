74b5a2aafe0fb6390cd27ce3eade2cf25fdfdd9f
Sahil <leviack629@gmail.com>
2023-08-20 13:09:16 +0530

    done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,183 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { useState } from "react";
+import CountUp from "react-countup";
 
-const inter = Inter({ subsets: ['latin'] })
+import {
+  differenceInDays,
+  differenceInMonths,
+  differenceInYears,
+} from "date-fns";
+import icon from "../images/icon-arrow.svg";
+import Head from "next/head";
+
+const poppins = Poppins({ subsets: ["latin"], weight: "700" });
 
 export default function Home() {
+  const [days, setDays] = useState("");
+  const [months, setMonths] = useState("");
+  const [years, setYears] = useState("");
+  const [diffInDays, setDiffInDays] = useState("");
+  const [diffInMonths, setDiffInMonths] = useState("");
+  const [diffInYears, setDiffInYears] = useState("");
+  const [inputsFilled, setInputsFilled] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "day") setDays(value);
+    if (name === "month") setMonths(value);
+    if (name === "year") setYears(value);
+
+
+      // Check if all input fields have values
+    if (days && months && years) {
+      setInputsFilled(true);
+    } else {
+      setInputsFilled(false);
+    }
+  };
+
+  const handleClick = () => {
+    const calculatedDiffInDays = differenceInDays(
+      new Date(years, months - 1, 31),
+      new Date(years, months - 1, days)
+    );
+
+    const calculatedDiffInMonths = differenceInMonths(
+      new Date(years, 11, 31),
+      new Date(years, months - 1, days)
+    );
+
+    const calculatedDiffInYears = differenceInYears(
+      new Date(2023, 11, 31),
+      new Date(years, months - 1, days)
+    );
+
+    setDiffInDays(calculatedDiffInDays);
+    setDiffInMonths(calculatedDiffInMonths);
+    setDiffInYears(calculatedDiffInYears);
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <Head>
+        <title>Age Calculator</title>
+      </Head>
+      <main
+        className={`lg:flex lg:items-center lg:justify-center lg:h-screen ${poppins.className}`}
+      >
+        <div className="max-w-lg bg-white rounded rounded-br-[40%] shadow p-8 mx-auto">
+          <div>
+            <form className="flex gap-4">
+              <article>
+                <label
+                  htmlFor="day"
+                  className={`${days > 31 && "text-red-500"}`}
+                >
+                  day
+                </label>
+                <input
+                  type="number"
+                  name="day"
+                  id="day"
+                  placeholder="DD"
+                  min="1"
+                  max="31"
+                  value={days}
+                  onChange={handleChange}
+                />
+                {days > 31 && (
+                  <small className="text-red-500 font-thin block">
+                    Must be a valid day
+                  </small>
+                )}
+              </article>
+              <article>
+                <label
+                  htmlFor="month"
+                  className={`${months > 12 && "text-red-500"}`}
+                >
+                  month
+                </label>
+                <input
+                  type="number"
+                  name="month"
+                  id="month"
+                  placeholder="MM"
+                  min="1"
+                  max="12"
+                  value={months}
+                  onChange={handleChange}
+                />
+                {months > 12 && (
+                  <small className="text-red-500 font-thin block">
+                    Must be a valid month
+                  </small>
+                )}
+              </article>
+              <article>
+                <label
+                  htmlFor="year"
+                  className={`${years > 2023 && "text-red-500"}`}
+                >
+                  year
+                </label>
+                <input
+                  type="number"
+                  name="year"
+                  id="year"
+                  placeholder="YYYY"
+                  min="1900"
+                  max="2023"
+                  value={years}
+                  onChange={handleChange}
+                />
+                {years > 2023 && (
+                  <small className="text-red-500 font-thin block">
+                    Must be a valid year
+                  </small>
+                )}
+                {/* {years < 1900 && years > 0 && <small className="text-red-500 font-thin block">You are too old to calculate your age</small>} */}
+              </article>
+            </form>
+          </div>
+
+          <div className="relative mt-10">
+            <hr className="border border-slate-400" />
+            <button
+              type="button"
+              className="bg-purple-600 rounded-full absolute w-30 h-30 p-2 right-0 -top-7"
+              onClick={handleClick}
+              disabled={!inputsFilled}
+            >
+              <Image src={icon} width={40} height={40} alt="Arrow Icon" />
+            </button>
+          </div>
+
+          <div className="mt-10">
+            <ul className="flex flex-col gap-2">
+              <li className="text-5xl italic">
+                <span className="text-purple-600 text-5xl">
+                  <CountUp start={0} end={diffInYears || 0} duration={1} />
+                </span>{" "}
+                years
+              </li>
+              <li className="text-5xl italic">
+                <span className="text-purple-600 text-5xl">
+                  <CountUp start={0} end={diffInMonths || 0} duration={1} />
+                </span>{" "}
+                months
+              </li>
+              <li className="text-5xl italic">
+                <span className="text-purple-600 text-5xl">
+                  <CountUp start={0} end={diffInDays || 0} duration={1} />
+                </span>{" "}
+                days
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </main>
+    </>
+  );
 }
